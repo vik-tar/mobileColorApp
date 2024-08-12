9b85687eb34ed08bbad29111e3b120850ff99a7e
vik-tar <zlatanero91@gmail.com>
2024-08-12 12:14:43 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+const symbolsToColorGenerate = '0123456789ABCDEF';
 
 export default function App() {
+  const [bgColor, setBgColor] = useState('#FFF');
+
+  function getRandomColor() {
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += symbolsToColorGenerate[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  const handlePress = () => {
+    const newColor = getRandomColor();
+    setBgColor(newColor);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <TouchableOpacity
+      style={[styles.container, { backgroundColor: bgColor }]}
+      onPress={handlePress}
+    >
+      <Text style={styles.text}>Hello there</Text>
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+    color: '#000',
+  },
+  colorCode: {
+    marginTop: 10,
+    fontSize: 18,
+    color: '#000',
   },
 });
+
